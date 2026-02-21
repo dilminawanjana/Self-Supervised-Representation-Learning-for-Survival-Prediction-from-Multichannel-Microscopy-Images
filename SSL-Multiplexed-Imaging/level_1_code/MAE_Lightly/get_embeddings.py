23aa8ffd79052388d2fe2103adf399d73109de4d
dilminawanjana <wmdnawanjana@gmail.com>
2026-02-21 12:35:35 +0100

    Increase default max_epochs and batch_size values

--- SSL-Multiplexed-Imaging/level_1_code/MAE_Lightly/get_embeddings.py
+++ SSL-Multiplexed-Imaging/level_1_code/MAE_Lightly/get_embeddings.py
@@ -86,12 +86,12 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Argument Parser for your script')
     
     parser.add_argument('--num_workers', type=int, default=7, help='Number of workers for data loading (default: 7)')
-    parser.add_argument('--max_epochs', type=int, default=5, help='Maximum number of epochs (default: 5)')
+    parser.add_argument('--max_epochs', type=int, default=100, help='Maximum number of epochs (default: 100)')
     parser.add_argument('--lr', type=float, default=6e-2, help='Learning rate (default: 6e-2)')
     parser.add_argument('--mask_ratio', type=float, default=0.75, help='Momentum (default: 0.9)')
     
     parser.add_argument('--input_size', type=int, default=256, help='Input size (default: 256)')
-    parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training (default: 128)')
+    parser.add_argument('--batch_size', type=int, default=128, help='Batch size for training (default: 128)')
     
     parser.add_argument('--dataset', type=str, default='', help='Name of the dataset')
     parser.add_argument('--data_path', type=str, default="")
